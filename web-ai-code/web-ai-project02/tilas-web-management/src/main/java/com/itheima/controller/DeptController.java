d317fb04f81547c34d4020d311d34e8cf55ac0dd
zn <16239955+zn_1109@user.noreply.gitee.com>
2025-11-05 21:48:48 +0800

    添加了README

--- web-ai-code/web-ai-project02/tilas-web-management/src/main/java/com/itheima/controller/DeptController.java
+++ web-ai-code/web-ai-project02/tilas-web-management/src/main/java/com/itheima/controller/DeptController.java
@@ -3,48 +3,55 @@ package com.itheima.controller;
 import com.itheima.pojo.Dept;
 import com.itheima.pojo.Result;
 import com.itheima.service.DeptService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Insert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class DeptController {
+
+//    private static final Logger log = LoggerFactory.getLogger(DeptController.class);
+
     @Autowired
     private DeptService deptService;
 //    @RequestMapping(value = "/depts",method = RequestMethod.GET)
     @GetMapping("/depts")
     public Result list(){
-        System.out.println("查询全部的部门数据");
+        log.info("查询全部的部门数据");
         List<Dept> deptList = deptService.findAll();
         return Result.success(deptList);
     }
 
     @DeleteMapping("/depts")
     public Result delete(Integer id){
-        System.out.println("删除部门数据:" + id);
+        log.info("根据id删除部门数据:{}", id);
         deptService.deleteById(id);
         return Result.success();
     }
 
     @PostMapping("/depts")
     public Result add(@RequestBody Dept dept){
-        System.out.println("添加部门数据:" + dept);
+        log.info("添加部门数据:{}",dept);
         deptService.insert(dept);
         return Result.success();
     }
 
     @GetMapping("/depts/{id}")
     public Result selectById(@PathVariable Integer id){
-        System.out.println("查询id对应的数据："+id);
+        log.info("查询id对应的数据：{}",id);
         Dept dept = deptService.selectById(id);
         return Result.success(dept);
 
     }
     @PutMapping("/depts")
     public Result update(@RequestBody Dept dept){
-        System.out.println("修改部门数据:" + dept);
+        log.info("修改部门数据:{}",dept);
         deptService.update(dept);
         return Result.success();
     }
