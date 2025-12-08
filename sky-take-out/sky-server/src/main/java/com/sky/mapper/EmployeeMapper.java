f4ee705a5979b152e40d9adb2fc835bb0c53865a
izhengnan <2062476522@qq.com>
2025-12-08 21:50:03 +0800

    完成了苍穹外卖添加员工接口

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,11 @@
 package com.sky.mapper;
 
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDateTime;
 
 @Mapper
 public interface EmployeeMapper {
@@ -15,4 +18,6 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+    @Insert("insert into employee (username, name, phone, sex, id_number, create_time, update_time, create_user, update_user, status, password) values (#{username}, #{name}, #{phone}, #{sex}, #{idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser}, #{status}, #{password})")
+    void saveEmployee(Employee employee);
 }
