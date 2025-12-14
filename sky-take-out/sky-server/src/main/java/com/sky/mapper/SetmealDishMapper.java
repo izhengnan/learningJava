bd3b9798b4b2e3c2dfccc18461a104183dd5d496
izhengnan <2062476522@qq.com>
2025-12-14 15:48:39 +0800

    新增套餐管理功能
    - 实现套餐信息的添加接口
    - 实现套餐菜品关联信息的批量插入
    - 完成套餐新增业务逻辑处理
    - 添加套餐控制器及服务层实现
    - 配置套餐相关 MyBatis 映射文件
    - 引入套餐实体类及相关 DTO 数据传输对象

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,11 +1,15 @@
 package com.sky.mapper;
 
+import com.sky.entity.SetmealDish;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Mapper
 public interface SetmealDishMapper {
 
     ArrayList<Long> getSetmealIdsByDishIds(ArrayList<Long> dishIds);
+
+    void addSetmealDishes(List<SetmealDish> setmealDishes);
 }
---
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -0,0 +1,44 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.SetmealDTO;
+import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
+import com.sky.mapper.SetmealDishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Slf4j
+public class SetmealServiceImpl implements SetmealService {
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
+
+
+    @Override
+    public void addSetmeal(SetmealDTO setmealDTO) {
+        Setmeal setmeal = new Setmeal();
+        BeanUtils.copyProperties(setmealDTO,setmeal);
+        setmeal.setCreateTime(LocalDateTime.now());
+        setmeal.setUpdateTime(LocalDateTime.now());
+        setmeal.setCreateUser(BaseContext.getCurrentId());
+        setmeal.setUpdateUser(BaseContext.getCurrentId());
+        setmealMapper.addSetmeal(setmeal);
+
+        List<SetmealDish> setmealDishes = setmealDTO.getSetmealDishes();
+        if (setmealDishes != null && !setmealDishes.isEmpty()){
+            setmealDishes.forEach(setmealDish -> setmealDish.setSetmealId(setmeal.getId()));
+        }
+        setmealDishMapper.addSetmealDishes(setmealDishes);
+    }
+}
